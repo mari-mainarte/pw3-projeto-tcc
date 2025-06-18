3024e94bb80c03d3685709477459d3793b2ce062
mari-mainarte <mariana.mainarte@etec.sp.gov.br>
2025-06-18 19:46:39 -0300

    :sparkles: Feat: Começando componente funcionalidade

--- pw3-projeto-tcc/src/app/funcionalidade/funcionalidade.component.ts
+++ pw3-projeto-tcc/src/app/funcionalidade/funcionalidade.component.ts
@@ -1,11 +1,66 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-funcionalidade',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './funcionalidade.component.html',
   styleUrl: './funcionalidade.component.css'
 })
 export class FuncionalidadeComponent {
+  cadastroForm : FormGroup;
+  criarBtn : boolean = false
+  nome = "";
+  email = "";
+  rm = 0;
+  telefone = "";
+  chamadosAbertos = 0;
+  chamadosConcluidos = 0;
+  senha = "";
+
+  usuarios: any[] = [
+    {"nome": "Marion", "email": "marion@gmail.com", "rm": 248533, "telefone": "(11) 99356-3948","chamadosAbertos": 10, "chamadosConcluidos": 8, "senha": "123456"},
+    {"nome": "Rogério", "email": "rogerio@gmail.com", "rm": 748659, "telefone": "(11) 90749-2172","chamadosAbertos": 7, "chamadosConcluidos": 4, "senha": "789012"}
+  ];
+
+  constructor(private fb: FormBuilder){
+    this.cadastroForm = this.fb.group({
+      nome : ['', [Validators.required]],
+      email : ['', [Validators.required]],
+      rm : ['', [Validators.required]],
+      telefone : ['', [Validators.required]],
+      chamadosAbertos : ['', [Validators.required]],
+      chamadosConcluidos : ['', [Validators.required]],
+      senha : ['', [Validators.required]],
+    });
+  }
+
+  submit() {
+    if (this.cadastroForm.valid) {
+      alert('Formulário válido');
+    } else {
+      alert('Formulário inválido');
+    }
+  }
+
+  criarUser(){
+    this.criarBtn = !this.criarBtn;
+  }
+
+  OnCadastrar(){
+    this.nome = this.cadastroForm.get('nome')?.value;
+    this.email = this.cadastroForm.get('email')?.value;
+    this.rm = this.cadastroForm.get('rm')?.value;
+    this.telefone = this.cadastroForm.get('telefone')?.value;
+    this.chamadosAbertos = this.cadastroForm.get('chamadosAbertos')?.value;
+    this.chamadosConcluidos = this.cadastroForm.get('chamadosConcluidos')?.value;
+    this.senha = this.cadastroForm.get('senha')?.value;
+    this.usuarios.push(
+      {"nome": this.nome, "email": this.email, "rm": this.rm, 
+        "telefone": this.telefone, "chamadosAbertos": this.chamadosAbertos, 
+        "chamadosConcluidos": this.chamadosConcluidos, "senha": this.senha
+      }
+    )
+  }
 
 }
--- pw3-projeto-tcc/src/app/home/home.component.ts
+++ pw3-projeto-tcc/src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { FuncionalidadeComponent } from "../funcionalidade/funcionalidade.component";
+import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [FuncionalidadeComponent],
+  imports: [FuncionalidadeComponent, HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
