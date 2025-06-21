7160b48301f8ac47b3c32a27615ac66953de6866
pan-it <paolacapaquira444@gmail.com>
2025-06-20 22:39:47 -0300

    :sparkles:Feat: Mexer no css do login, contato e funcionalidade

--- pw3-projeto-tcc/src/app/header/header.component.ts
+++ pw3-projeto-tcc/src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ContatoComponent } from "../contato/contato.component";
+import { SobreComponent } from "../sobre/sobre.component";
 
 @Component({
   selector: 'app-header',
-  imports: [ContatoComponent],
+  imports: [ContatoComponent, SobreComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
